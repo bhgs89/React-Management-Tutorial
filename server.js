f74ec3563cf12e395e70eae435d2d87805034145
Han <han.baek.program@gmail.com>
2019-08-17 02:51:51 -0700

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// file에 접근 할 수 있는 library
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +9,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// database.json file 읽어오기
+const data = fs.readFileSync('./database.json');
+
+// 해당 환경설정 데이터를 가져온다
+const conf = JSON.parse(data);
+
+// mysql library 이용하기
+const mysql = require('mysql');
+
+// 연결과 관련한 변수 설정
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+// 실제로 연결
+connection.connect();
+
 // 클라이언트가 server -> /api/customers 에 접속했을 때
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: 'https://placeimg.com/64/64/1',
-      name: '나동빈',
-      birthday: '961222',
-      gender: '남자',
-      job: '대학생',
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/64/64/2',
-      name: '홍길동',
-      birthday: '960305',
-      gender: '남자',
-      job: '프로그래머',
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/64/64/3',
-      name: '이순신',
-      birthday: '921205',
-      gender: '남자',
-      job: '디자이너',
-    },
-  ]);
+  connection.query('SELECT * FROM CUSTOMER', (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
